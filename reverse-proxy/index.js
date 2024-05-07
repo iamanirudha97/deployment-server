51ea346233be4a6fd5c261e30c505b9e0e9742f2
iamanirudha97 <sagarlm10@gmail.com>
2024-05-06 20:48:53 -0400

    reverse proxy implemented

---
+++ reverse-proxy/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const httpProxy =require('http-proxy')
+
+const app = express();
+const PORT = 8000;
+
+const base_Url = `https://vercel-clone-project-output.s3.us-east-2.amazonaws.com/__outputs`
+const proxy = httpProxy.createProxy();
+
+app.use((req, res) => {
+    const hostname = req.hostname;
+    const subdomain = hostname.split('.')[0].toUpperCase();
+    console.log(subdomain);
+
+    const resolvesTo = `${base_Url}/${subdomain}`
+
+    return proxy.web(req, res, {target: resolvesTo, changeOrigin: true });
+})
+
+proxy.on('proxyReq', (proxyReq, req, res) => {
+    const url = req.url;
+    if(url === '/'){
+        proxyReq.path += 'index.html'
+    }
+    return proxyReq
+})
+
+app.listen(PORT, ()=> {
+    console.log(`REVERSE PROXY SERVER RUNNING ON ${PORT}`);
+})
